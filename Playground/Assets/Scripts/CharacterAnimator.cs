bdfb768168688636deedd23263cbe41234e4c3ff
FrancescoLenolli <francescopietrolenolli@gmail.com>
2021-05-31 15:37:23 +0200

    Small cleanup

--- Playground/Assets/Scripts/CharacterAnimator.cs
+++ Playground/Assets/Scripts/CharacterAnimator.cs
@@ -2,13 +2,10 @@
 
 public class CharacterAnimator : MonoBehaviour
 {
-    public Vector2 xValueRange = Vector2.zero;
     public float maxAnimationValue = 2.0f;
-    //public Vector2 zValueRange = Vector2.zero;
 
     private Animator animator;
     private int velocityXHash;
-    //private int velocityZHash;
     private float velocityX;
     private float velocityZ;
 
@@ -16,7 +13,6 @@ public class CharacterAnimator : MonoBehaviour
     {
         this.animator = animator;
         velocityXHash = Animator.StringToHash("VelocityX");
-        //velocityZHash = Animator.StringToHash("VelocityZ");
         velocityX = 0.0f;
         velocityZ = 0.0f;
     }
@@ -24,9 +20,7 @@ public class CharacterAnimator : MonoBehaviour
     public void HandleAnimation(float inputValue, bool isRunning)
     {
         velocityX = Mathf.Clamp(inputValue, 0.0f, isRunning ? maxAnimationValue : maxAnimationValue * 0.5f);
-        //velocityZ = Mathf.Abs(Mathf.Clamp(isRunning ? inputValue.z * 2 : inputValue.z, zValueRange.x, zValueRange.y));
 
         animator.SetFloat(velocityXHash, velocityX);
-        //animator.SetFloat(velocityZHash, velocityZ);
     }
 }
