7e8ad42d685b90237fd06098aae3e013e8060da6
FrancescoLenolli <francescopietrolenolli@gmail.com>
2021-06-04 16:57:33 +0200

    Random punching animation with substate machine

---
+++ Playground/Assets/Scripts/RandomAnimation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RandomAnimation : StateMachineBehaviour
+{
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+
+        animator.SetInteger("Punch_Index", Random.Range(0, 3));
+    }
+}
