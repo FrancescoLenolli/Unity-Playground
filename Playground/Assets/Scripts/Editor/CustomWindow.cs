254b47ed776d055f4113b151af05e730f802623d
FrancescoLenolli <francescopietrolenolli@gmail.com>
2021-07-26 15:17:08 +0200

    Study on Custom Windows
    I want to make a tool for a "UI Framework"

---
+++ Playground/Assets/Scripts/Editor/CustomWindow.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+class CustomWindow : EditorWindow
+{
+    GameObject newObject;
+    Transform parent;
+
+    [MenuItem("Custom/Spawner")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(CustomWindow));
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.LabelField("Select Object to Spawn", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("Empty Object"))
+        {
+            SpawnObject();
+        }
+    }
+
+    private void SpawnObject()
+    {
+        parent = Selection.activeTransform;
+
+        if (!parent)
+            PrefabUtility.InstantiatePrefab(GetObject());
+        else
+            PrefabUtility.InstantiatePrefab(GetObject(), parent);
+    }
+
+    private GameObject GetObject()
+    {
+        if (!newObject)
+            newObject = Resources.Load<GameObject>("Empty Object");
+
+        return newObject;
+    }
+}
